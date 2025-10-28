986f4aeaf7acd8a69c0a3fb7d8f57eda4f937519
Asadjamil21 <jamilasad276@gmail.com>
2025-10-28 09:43:10 +0500

    changes made

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 TAX_RATE: float = float(os.getenv("TAX_RATE", "0.06"))
-
+#configuration
 MONGODB_URI = os.getenv(
     "MONGODB_URI")
 DB_NAME      = os.getenv("DB_NAME", "voice_agents")
